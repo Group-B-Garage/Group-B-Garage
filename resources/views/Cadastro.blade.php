0e12fd3405db0fc3fc84d381aa1cbe497f0744d1
halysonSA <halysonSA@gmail.com>
2022-01-17 19:18:50 -0300

    Tela de cadastro de carros

---
+++ resources/views/Cadastro.blade.php
@@ -0,0 +1,116 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+     <title>GB Garage</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+   
+  </head>
+  <body>
+
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid d-flex justify-content-center">
+    <span class="navbar-brand mb-0 h1 ">Cadastrar Veículo</span>
+  </div>
+</nav>
+
+
+<div class="card m-5 p-3 shadow-lg  " style="min-height: 200px;
+">
+<div class="row g-3">
+    <div class="col-sm-6">
+      <label for="firstName" class="form-label">????????</label>
+      <input type="text" class="form-control" id="####" placeholder="" value="" required>
+      <div class="invalid-feedback">
+        ??????????
+      </div>
+    </div>
+
+    <div class="col-sm-6">
+      <label for="lastName" class="form-label">??????????</label>
+      <input type="text" class="form-control" id="####" placeholder="" value="" required>
+      <div class="invalid-feedback">
+        ?????????
+      </div>
+    </div>
+
+    <div class="col-12">
+      <label for="username" class="form-label">?????????</label>
+      <div class="input-group has-validation">
+        
+        <input type="text" class="form-control" id="####" placeholder="não?" required>
+      <div class="invalid-feedback">
+         ????????
+        </div>
+      </div>
+    </div>
+
+    <div class="col-12">
+      <label for="####" class="form-label">?????????? </label>
+      <input type="####" class="form-control" id="email" placeholder="alguma coisa ae">
+      <div class="invalid-feedback">
+        ??????????
+      </div>
+    </div>
+
+    <div class="col-12">
+      <label for="address" class="form-label">????????</label>
+      <input type="text" class="form-control" id="address" placeholder="sim?" required>
+      <div class="invalid-feedback">
+        Please enter your shipping address.
+      </div>
+    </div>
+
+    
+
+    <div class="col-md-5">
+      <label for="country" class="form-label">Marca do veiculo pode ser aqui.</label>
+      <select class="form-select" id="####" required>
+        <option value="">Selecionar</option>
+        <option value="">Ferrari</option>
+        <option value="">Ford</option>
+        <option value="">Audi</option>
+        
+      </select>
+      <div class="invalid-feedback">
+        Please select a marca:)
+      </div>
+    </div>
+
+    <div class="col-md-4">
+      <label for="state" class="form-label">pode ser a cor...</label>
+      <select class="form-select" id="####" required>
+        <option value="">Selecionar</option>
+        <option>Vemelho</option>
+        <option>branco</option>
+      </select>
+      <div class="invalid-feedback">
+        Please provide a valid state.
+      </div>
+    </div>
+
+    <div class="col-md-3">
+      <label for="zip" class="form-label">Imagem</label>
+      <input type="text" class="form-control" id="####" placeholder="link da imagem" required>
+      <div class="invalid-feedback">
+        aon aonaon
+      </div>
+    </div>
+  
+  </div>
+  <button class="w-100 btn btn-dark btn-md mt-3" type="submit">Salvar</button>
+    
+</div>
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
