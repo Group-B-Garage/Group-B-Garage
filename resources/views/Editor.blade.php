0ad5f7362b5aa3a7bcad71ab566ee892c538a5e9
halysonSA <halysonSA@gmail.com>
2022-01-23 18:39:32 -0300

    Tela de Admin

---
+++ resources/views/Editor.blade.php
@@ -0,0 +1,165 @@
+<!doctype html>
+<html lang="en">
+  <head>
+     <title>GB Garage</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+        .botoeseditar{
+            border-radius:6px 0px 0px 6px;
+        
+        }
+        .botoesexcluir{
+            border-radius:0px 6px 6px 0px;
+        }
+        .linktext{
+            text-decoration: none;
+        }
+
+    </style>
+   
+  </head>
+  <body>
+
+<nav class="navbar col-xs navbar-dark bg-dark" style="min-width:900px">
+  <div class="container-fluid d-flex justify-content-center">
+  
+    <span class="navbar-brand mb-0 h1 ">Controle Geral</span>
+    
+  </div>
+</nav>
+
+<div class="card  m-5 p-3 shadow-lg  " style="min-width: 800px;min-height: 200px;
+">
+
+    <table class="table ">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">MODELO</th>
+            <th scope="col">MARCA</th>
+            <th scope="col">COR</th>
+            <th scope="col">KM</th>
+            <th scope="col">####</th>
+            
+
+          </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">codigo</th>
+                <td>Audi TT</td>
+                <td>Audi</td>
+                <td>Prata</td>
+                <td>87</td>
+                <td class="d-flex justify-content-center " >
+                    <a href="#"> <button type="button" class="btn btn-dark botoeseditar">Editar</button></a>
+                    <button type="button" class="btn btn-warning botoesexcluir">Excluir</button>
+                    
+                </td>
+                
+              </tr>
+              <tr>
+                <th scope="row">codigo</th>
+                <td>Audi TT</td>
+                <td>Audi</td>
+                <td>Prata</td>
+                <td>87</td>
+                <td class="d-flex justify-content-center " >
+                    <a href="#"> <button type="button" class="btn btn-dark botoeseditar">Editar</button></a>
+                    <button type="button" class="btn btn-warning botoesexcluir">Excluir</button>
+                    
+                </td>
+                
+              </tr>
+               <tr>
+                <th scope="row">codigo</th>
+                <td>Audi TT</td>
+                <td>Audi</td>
+                <td>Prata</td>
+                <td>87</td>
+                <td class="d-flex justify-content-center " >
+                    <a href="#"> <button type="button" class="btn btn-dark botoeseditar">Editar</button></a>
+                    <button type="button" class="btn btn-warning botoesexcluir">Excluir</button>
+                    
+                </td>
+                
+              </tr> <tr>
+                <th scope="row">codigo</th>
+                <td>Audi TT</td>
+                <td>Audi</td>
+                <td>Prata</td>
+                <td>87</td>
+                <td class="d-flex justify-content-center " >
+                    <a href="#"> <button type="button" class="btn btn-dark botoeseditar">Editar</button></a>
+                    <button type="button" class="btn btn-warning botoesexcluir">Excluir</button>
+                    
+                </td>
+                
+              </tr> <tr>
+                <th scope="row">codigo</th>
+                <td>Audi TT</td>
+                <td>Audi</td>
+                <td>Prata</td>
+                <td>87</td>
+                <td class="d-flex justify-content-center " >
+                    <a href="#"> <button type="button" class="btn btn-dark botoeseditar">Editar</button></a>
+                    <button type="button" class="btn btn-warning botoesexcluir">Excluir</button>
+                    
+                </td>
+                
+              </tr> <tr>
+                <th scope="row">codigo</th>
+                <td>Audi TT</td>
+                <td>Audi</td>
+                <td>Prata</td>
+                <td>87</td>
+                <td class="d-flex justify-content-center " >
+                    <a href="#"> <button type="button" class="btn btn-dark botoeseditar">Editar</button></a>
+                    <button type="button" class="btn btn-warning botoesexcluir">Excluir</button>
+                    
+                </td>
+                
+              </tr> <tr>
+                <th scope="row">codigo</th>
+                <td>Audi TT</td>
+                <td>Audi</td>
+                <td>Prata</td>
+                <td>87</td>
+                <td class="d-flex justify-content-center " >
+                    <a href="#"> <button type="button" class="btn btn-dark botoeseditar">Editar</button></a>
+                    <button type="button" class="btn btn-warning botoesexcluir">Excluir</button>
+                    
+                </td>
+                
+              </tr> <tr>
+                <th scope="row">codigo</th>
+                <td>Audi TT</td>
+                <td>Audi</td>
+                <td>Prata</td>
+                <td>87</td>
+                <td class="d-flex justify-content-center " >
+                    <a href="#"> <button type="button" class="btn btn-dark botoeseditar">Editar</button></a>
+                    <button type="button" class="btn btn-warning botoesexcluir">Excluir</button>
+                    
+                </td>
+                
+              </tr>
+        </tbody>
+      </table>
+
+        <a href="#"> <button type="button" class="btn w-100 btn-dark">Adicionar Carro</button></a>
+  
+  
+</div>
+
+
+
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
